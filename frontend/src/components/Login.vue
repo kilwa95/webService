619f98fce2e3928b8fe30c7303d6df0be43cf382
Eddie Saliby <eddysaliby@gmail.com>
2021-12-15 12:13:17 +0100

    [front] - Fix Login Form

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="vue-tempalte">
-    <form>
+    <form @submit.prevent="handleSubmit">
       <h3>Sign In</h3>
       <div class="form-group">
         <label>Email address</label>
-        <input type="email" class="form-control form-control-lg" />
+        <input
+          name="email"
+          type="email"
+          class="form-control form-control-lg"
+          @keydown.prevent.stop.enter="avoidEnter"
+          :value="email"
+          @input="handleChange"
+        />
       </div>
       <div class="form-group">
         <label>Password</label>
-        <input type="password" class="form-control form-control-lg" />
+        <input
+          name="password"
+          type="password"
+          class="form-control form-control-lg"
+          @keydown.prevent.stop.enter="avoidEnter"
+          v-model="password"
+          @input="handleChange"
+        />
       </div>
-      <button type="submit" class="btn btn-dark btn-lg btn-block">
-        Sign In
-      </button>
-
+      <input
+        value="Sign in"
+        type="submit"
+        class="btn btn-dark btn-lg btn-block"
+      />
       <p class="forgot-password text-right mt-2 mb-4">
         <router-link to="/forgot-password">Forgot password ?</router-link>
       </p>
@@ -21,10 +36,25 @@
   </div>
 </template>
 
+
 <script>
 export default {
   data() {
-    return {};
+    return {
+      email: "",
+      password: "",
+    };
   },
+  methods: {
+    handleChange: function (event) {
+      this[event.target.name] = event.target.value;
+    },
+    handleSubmit: function (event) {
+      const data = new FormData(event.target);
+      console.log(data);
+    },
+    avoidEnter: () => console.log("Enter avoided"),
+  },
+  components: {},
 };
 </script>
---
+++ frontend/src/components/lib/Form.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input
+      name="username"
+      type="text"
+      @keydown.prevent.stop.enter="avoidEnter"
+      :value="username"
+      @input="handleChange"
+    />
+    <input name="password" type="text" v-model="password" />
+    <input type="submit" value="Submit" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Form",
+  data: () => ({
+    username: "",
+    password: "",
+  }),
+  methods: {
+    handleChange: function (event) {
+      this[event.target.name] = event.target.value;
+    },
+    handleSubmit: function (event) {
+      const data = new FormData(event.target);
+      console.log(data);
+    },
+    avoidEnter: () => console.log("Enter avoided"),
+  },
+};
+</script>
