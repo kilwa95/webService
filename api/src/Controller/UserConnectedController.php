da31b53c3091435ff367a179bb00cd85c746c436
khaled <lga.sig.khaled@gmail.com>
2022-01-19 12:26:03 +0100

    userCurrente

--- api/src/Controller/UserConnectedController.php
+++ api/src/Controller/UserConnectedController.php
@@ -3,21 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class UserConnectedController
 {
     private $tokenStorage;
 
-    public function __construct(TokenStorageInterface $tokenStorage)
+    public function __construct(Security $tokenStorage)
     {
         $this->tokenStorage = $tokenStorage;
     }
 
     public function __invoke()
     {
-        $user = $this->tokenStorage->getToken();
-        return $user;
+        // $user = $this->tokenStorage->getToken()->getUser();
     }
 
 }
